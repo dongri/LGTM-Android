025adfb4f72dd41ec65c01640e88403ddf2cc6ea
Dongri Jin <dongrify@gmail.com>
2017-11-27 18:09:32 +0900

    Fix

--- app/src/main/java/lol/lgtm/SubmitFragment.java
+++ app/src/main/java/lol/lgtm/SubmitFragment.java
@@ -1,6 +1,7 @@
 package lol.lgtm;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,12 +11,15 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Base64;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -30,6 +34,7 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
@@ -43,12 +48,11 @@ import java.util.Map;
 
 public class SubmitFragment extends Fragment {
 
-    private String url = "https://lgtm.lol/api/submit";
     private String urlUpload = "https://lgtm.lol/api/upload";
-    private EditText editText;
     private final static int RESULT_CAMERA = 1001;
+    private Button uploadButton;
     private ImageView imageView;
-    private Bitmap bitmap;
+    private TextView lgtmView;
 
     private Uri imageUri;
 
@@ -63,22 +67,15 @@ public class SubmitFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        editText = view.findViewById(R.id.plain_text_input);
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
 
-        Button button = view.findViewById(R.id.button_submit);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                submit(editText.getText().toString());
-            }
-        });
+        lgtmView = (TextView) view.findViewById(R.id.lgtm_text);
+        lgtmView.setVisibility(View.GONE);
 
         Button cameraButton = (Button) view.findViewById(R.id.button_camera);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 ContentValues cv = new ContentValues();
                 cv.put(MediaStore.Images.Media.TITLE, "My Picture");
                 cv.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
@@ -93,15 +90,16 @@ public class SubmitFragment extends Fragment {
 
         imageView = (ImageView) view.findViewById(R.id.item_image);
 
-        Button uploadButton = (Button) view.findViewById(R.id.button_upload);
+        uploadButton = (Button) view.findViewById(R.id.button_upload);
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String image = getStringImage(bitmap);
-                //passing the image to volley
-                SendImage(image);
+                View lgtmView = (View) view.findViewById(R.id.lgtm_image);
+                uploadButton.setEnabled(false);
+                SendImage(lgtmView);
             }
         });
+        uploadButton.setEnabled(false);
     }
 
     @Override
@@ -113,7 +111,8 @@ public class SubmitFragment extends Fragment {
                             getActivity().getContentResolver(), imageUri);
                     bitmap = resize(bitmap, 1000, 1000);
                     imageView.setImageBitmap(bitmap);
-                    this.bitmap = bitmap;
+                    lgtmView.setVisibility(View.VISIBLE);
+                    uploadButton.setEnabled(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -121,61 +120,35 @@ public class SubmitFragment extends Fragment {
         }
     }
 
-    private void submit(final String imageUrl) {
+    private void SendImage(final View v) {
         final Loading loading = new Loading(getActivity());
         loading.show();
-        StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            String result = jsonObject.getString("result");
-                            Toast.makeText(getContext(), result, Toast.LENGTH_SHORT).show();
-                            editText.setText("");
-                            loading.close();
-                        } catch (JSONException e) {
-                            // error
-                            Toast.makeText(getContext(), "Error:" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                            loading.close();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //error
-                        Toast.makeText(getContext(), "Error:" + error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                        loading.close();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                // パラメータ設定
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("url", imageUrl);
-                return params;
-            }
-        };
 
-        // 送信
-        Controller.getPermission().addToRequestQueue(request);
-    }
+        v.setDrawingCacheEnabled(true);
+        v.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+        v.buildDrawingCache(true);
+        Bitmap b = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
+        v.setVisibility(View.GONE);
+        final String image = getStringImage(b);
 
-
-    private void SendImage(final String image) {
-        final Loading loading = new Loading(getActivity());
-        loading.show();
         final StringRequest stringRequest = new StringRequest(Request.Method.POST, urlUpload,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonObject = new JSONObject(response);
-                            Toast.makeText(getContext(), "DONE", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getContext(), "Successfully", Toast.LENGTH_LONG).show();
                             loading.close();
                             imageView.setImageBitmap(null);
+                            uploadButton.setEnabled(false);
+                            lgtmView.setVisibility(View.GONE);
                         } catch (JSONException e) {
+                            loading.close();
+                            uploadButton.setEnabled(false);
+                            lgtmView.setVisibility(View.GONE);
                             Toast.makeText(getContext(), "Error" + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
                         }
                     }
@@ -183,7 +156,10 @@ public class SubmitFragment extends Fragment {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getContext(), "No internet connection", Toast.LENGTH_LONG).show();
+                        loading.close();
+                        uploadButton.setEnabled(false);
+                        lgtmView.setVisibility(View.GONE);
+                        Toast.makeText(getContext(), "Error: " + error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
                     }
                 }) {
             @Override
@@ -202,7 +178,7 @@ public class SubmitFragment extends Fragment {
 
     public String getStringImage(Bitmap bmp) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        bmp.compress(Bitmap.CompressFormat.PNG, 80, baos);
         byte[] imageBytes = baos.toByteArray();
         String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
         return encodedImage;
@@ -235,6 +211,30 @@ public class SubmitFragment extends Fragment {
 
         return resizeBitmap;
 
+    }
+
+
+
+    /**
+     * dpからpixelへの変換
+     * @param dp
+     * @param context
+     * @return float pixel
+     */
+    public static float convertDp2Px(float dp, Context context){
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return dp * metrics.density;
+    }
+
+    /**
+     * pixelからdpへの変換
+     * @param px
+     * @param context
+     * @return float dp
+     */
+    public static float convertPx2Dp(int px, Context context){
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return px / metrics.density;
     }
 
 }
